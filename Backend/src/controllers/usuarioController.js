befd0da2fbd200577ee5fece1db1d66a7cdc3c38
cristinarestrepo82232-del <cristina_restrepo82232@elpoli.edu.co>
2026-05-16 01:18:22 -0500

    Ajuste de puerto para despliegue

--- Backend/src/controllers/usuarioController.js
+++ Backend/src/controllers/usuarioController.js
@@ -3,7 +3,7 @@ const pool = require('../config/db'); // Ajusta esta ruta según dónde tengas t
 const getUsuarios = async (req, res, next) => {
     try {
         // Traemos el ID y el email. Le ponemos el alias 'nombre_usuario' para que React lo lea
-        const [usuarios] = await pool.query('SELECT id_usuario, email AS nombre_usuario FROM usuarios');
+        const [usuarios] = await pool.query('SELECT id_usuario, email AS nombre_usuario FROM usuarios WHERE fk_rol = 3');
         
         res.status(200).json(usuarios);
     } catch (error) {
